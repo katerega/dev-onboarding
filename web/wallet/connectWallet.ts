cf036e42d9116743f98b2764bb1c130d6ea38806
katerega <8218kelmo@gmail.com>
2025-08-14 14:19:39 +0300

    Update connectWallet.ts

--- web/wallet/connectWallet.ts
+++ web/wallet/connectWallet.ts
@@ -1,1 +1,20 @@
-connectWallet.ts
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import { injectedWallet } from '@rainbow-me/rainbowkit/wallets';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { bscTestnet, baseGoerli, polygonMumbai } from 'wagmi/chains';
+
+const connectors = connectorsForWallets([
+  { groupName: 'Recommended', wallets: [injectedWallet()] },
+]);
+
+const { chains, provider } = configureChains(
+  [bscTestnet, baseGoerli, polygonMumbai],
+  [publicProvider()]
+);
+
+export const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+});
