865a762c8faa28654b8100c93f581c5486fd220a
katerega <8218kelmo@gmail.com>
2025-08-14 14:00:36 +0300

    Update deploy.js

--- starter-kits/swap-dex/deploy.js
+++ starter-kits/swap-dex/deploy.js
@@ -1,2 +1,13 @@
-starter-kits
-deploy.js
+const hre = require("hardhat");
+
+async function main() {
+  const SwapDex = await hre.ethers.getContractFactory("SwapDex");
+  const swapDex = await SwapDex.deploy();
+  await swapDex.deployed();
+  console.log("SwapDex deployed to:", swapDex.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
